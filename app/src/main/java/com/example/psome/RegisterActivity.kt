733ef91a25497b994f718a4951fa558a02b71380
Gabriel CROSNIER DE BELLAISTRE <leirbag28000@gmail.com>
2021-09-22 09:33:32 +0200

    remaking start

--- app/src/main/java/com/example/psome/RegisterActivity.kt
+++ app/src/main/java/com/example/psome/RegisterActivity.kt
@@ -2,10 +2,12 @@ package com.example.psome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        Log.d("RegisterActivityLog","ActivityStarted")
     }
 }
